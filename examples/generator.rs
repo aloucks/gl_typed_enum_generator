37b6cef172c68297d6d2b20859e170d6df07bdb4
Aaron Loucks <aloucks@cofront.net>
2018-12-18 19:53:21 -0500

    Initial copy from gl_generator

---
+++ examples/generator.rs
@@ -0,0 +1,17 @@
+extern crate gl_generator;
+
+mod struct_gen;
+
+use struct_gen::StructGenerator;
+
+use gl_generator::{Api, Fallbacks, Profile, Registry};
+
+fn main() -> Result<(), Box<std::error::Error>> {
+
+    let registry = Registry::new(Api::Gl, (4, 5), Profile::Core, Fallbacks::All, []);
+    let out = std::io::stdout();
+    let mut out = out.lock();
+    registry.write_bindings(StructGenerator, &mut out)?;
+
+    Ok(())
+}
